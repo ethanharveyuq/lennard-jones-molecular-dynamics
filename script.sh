b70cfae1beefae13f32ea6ebb46145daa761a4c8
s4882205 <s4882205@login0.compute.eait.uq.edu.au>
2026-07-30 13:34:39 +1000

    Adjusted script to add user and removed gcc

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=milestone0
 #SBATCH --output=milestone0_%j.out
 #SBATCH --ntasks=1
+#SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=00:05:00
-#SBATCH --partition=<fill in>
-
-module load gcc
+#SBATCH --partition=cosc3500
+#SBATCH --account=cosc3500
 
 g++ -O2 -o md_project main.cpp
 ./md_project
