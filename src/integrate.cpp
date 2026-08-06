944b4e787213e53954479817315da1cad5caaa8d
Ethan Harvey <ethanjharvey06@gmail.com>
2026-08-06 12:52:11 +1000

    Added move and change velocities function based on velocity verlet

--- src/integrate.cpp
+++ src/integrate.cpp
@@ -1,5 +1,18 @@
 #include "../include/integrate.h"
 
-void move(System& sys, double dt);
-void update_velocities(System& sys, const std::vector<double>& fx_new,
-                        const std::vector<double>& fy_new, double dt);
+// uses velocity verlet
+
+void move(System& sys, double dt) {
+    for (int i = 0; i < sys.n; i++) {
+        sys.x[i] = sys.x[i] + sys.vx[i] * dt + 0.5 * sys.fx[i] * dt * dt;
+        sys.y[i] = sys.y[i] + sys.vy[i] * dt + 0.5 * sys.fy[i] * dt * dt;
+    }
+
+}
+
+void update_velocities(System& sys, const std::vector<double>& fx_new, const std::vector<double>& fy_new, double dt) {
+    for (int i = 0 ; i < sys.n; i++) {
+        sys.vx[i] = sys.vx[i] + 0.5 * (sys.fx[i] + fx_new[i]) * dt;
+        sys.vy[i] = sys.vy[i] + 0.5 * (sys.fy[i] + fy_new[i]) * dt;
+    }
+}
